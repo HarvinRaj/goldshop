19aee1d1d48a5d63d5f702c5f78cf6e7f40ef5b7
Harvin <harvinnnraj@gmail.com>
2025-01-22 00:20:40 +0800

    [GS-8] Done restructing user

--- internal/models/user_model.go
+++ internal/models/user_model.go
@@ -1,8 +1,16 @@
 package models
 
+import "time"
+
 type Users struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	UserID    int       `json:"user_id"`
+	UserName  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"lastName"`
+	RoleID    int       `json:"role_id"`
+	IsActive  bool      `json:"is_active"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
--- migrations/000003_user_roles.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE user_roles (
-    user_id INT NOT NULL,
-    role_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE,
-    PRIMARY KEY (user_id, role_id)
-);
