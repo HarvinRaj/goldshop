9ead1ac920c21ac952a1f831a4ef968d4fbe3762
Harvin <harvinnnraj@gmail.com>
2025-01-23 02:46:38 +0800

    add context

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,31 +1,22 @@
 package main
 
 import (
-	"context"
+	"log"
+
 	"github.com/HarvinRaj/goldshop/configs"
+	"github.com/HarvinRaj/goldshop/internal/ctxutils"
 	"github.com/HarvinRaj/goldshop/internal/db"
 	"github.com/HarvinRaj/goldshop/internal/routes"
-	_ "github.com/joho/godotenv/autoload"
-	"log"
-	"time"
 )
-
-var config *configs.Config
-
-func init() {
-
-	var err error
-
-	config, err = configs.LoadConfig("env.json")
-	if err != nil {
-		log.Fatalf("Failed to load config, %v", err)
-	}
-}
 
 func main() {
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	config, err := configs.LoadConfig("env.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := ctxutils.NewCTX()
 
 	database, err := db.NewDBConnection(ctx, config)
 	if err != nil {
